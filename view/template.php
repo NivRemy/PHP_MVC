41cf309e48eed4ec61ccdf52d2ae5e00b2a39f0f
Rémy NIVOIS <remynivois@adrar-formation.com>
2019-07-29 11:33:56 +0200

    added css

--- view/template.php
+++ view/template.php
@@ -8,6 +8,7 @@
 	<meta charset="UTF-8">
 	<title>Voyages</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -43,6 +44,7 @@
 	    </ul>
 	  </div>
 	</nav>
+	<br>
 
 	<!-- Affichage du contenu de la page -->
 	<?= $content;?>
